ceab9ceb1faf6453ee8d2896a6996f9c92c66cd9
Kirk Vander Kuy <kvk807@gmail.com>
2023-08-13 14:57:46 -0500

    connect mongodb to compass

--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,11 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+import connectDB from './config/db.js';
 import products from './data/products.js';
 const port = process.env.PORT || 4040;
+
+connectDB();
 
 const app = express();
 

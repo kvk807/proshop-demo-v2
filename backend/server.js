45b3a0fea313fa565b3065da3d4dab5f625b1091
Kirk Vander Kuy <kvk807@gmail.com>
2023-08-20 17:00:15 -0500

    added backend/middleware folder, added backkend/routes folder, moved productRoutes to routes folder from server.js

--- backend/server.js
+++ backend/server.js
@@ -4,24 +4,23 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 import connectDB from './config/db.js';
-import products from './data/products.js';
+import productRoutes from './routes/productRoutes.js';
+
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 const port = process.env.PORT || 4040;
 
 connectDB();
 
 const app = express();
+app.use(express.json());
+
+app.use('/api/products', productRoutes);
 
 app.get('/', (req, res) => {
   res.send('API is running...');
 });
 
-app.get('/api/products', (req, res) => {
-  res.json(products);
-});
-
-app.get('/api/products/:id', (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
